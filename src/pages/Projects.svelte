adee94ad49a97048409f5388601f4c46076ce7c3
briantkho <briantkho@gmail.com>
2023-05-11 19:48:38 -0700

    Projects expand on hover Elements still WIP

--- src/pages/Projects.svelte
+++ src/pages/Projects.svelte
@@ -6,26 +6,51 @@
 </script>
 
 <h1 class="text-7xl font-bold w-screen py-5 flex items-center justify-center">PROJECTS</h1>
-<div class="w-screen h-screen flex">
-	{#each data as project}
-		<div class="w-full h-full flex">
-			<img
-				src={project.image}
-				alt={project.title}
-				class="object-none h-screen w-[{imageRatio}] cursor-pointer hover:w-[133%]"
-			/>
-			<!-- !!! expand width on hover -->
-		</div>
-		<!-- <div class="w-full h-full backdrop-blur-md sticky" /> -->
-		<!-- <div class="w-screen h-screen overflow-hidden">
-			<h1 class="absolute">{project.title}</h1>
-			<div class="flex items-center justify-center">
-				<img
-					src={project.image}
-					alt={project.title}
-					class="object-none w-screen h-screen aspect-video max-w-none"
-				/>
+<div class="w-screen h-screen">
+	<div class="flex">
+		{#each data as project}
+			<div class="div-hover overflow-hidden">
+				<h1 class="absolute z-10">{project.title}</h1>
+				<div class="w-full overflow-hidden">
+					<div class="blur-hover bg-dark-background bg-opacity-25" />
+					<img src={project.image} alt={project.title} class="w-full object-none h-screen" />
+				</div>
 			</div>
-		</div> -->
-	{/each}
+		{/each}
+	</div>
 </div>
+
+<style lang="scss">
+	.div-hover {
+		width: 25%;
+		transition: width 0.25s ease-in-out;
+	}
+
+	h1 {
+		opacity: 0;
+		transition: all 0.25s ease-in-out;
+	}
+	.div-hover:hover h1 {
+		opacity: 1;
+	}
+
+	.div-hover:hover {
+		width: 40%;
+		cursor: pointer;
+		clip-path: inset(0 0 0 0);
+	}
+
+	.blur-hover {
+		opacity: 0;
+		transition: all 0.25s ease-in-out;
+		overflow: hidden;
+	}
+
+	.div-hover:hover .blur-hover {
+		opacity: 1;
+		position: absolute;
+		width: 40%;
+		height: 100%;
+		backdrop-filter: blur(10px);
+	}
+</style>
