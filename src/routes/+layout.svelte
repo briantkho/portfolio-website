5f55999d44627bd481d1d6b08f2fc1fdf208333b
briantkho <briantkho@gmail.com>
2024-09-13 23:57:13 -0700

    overflow hidden

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 
 <div class={currentTheme}>
 	<main
-		class="dark:fill-dark-ow dark:text-dark-ow text-light-black transition-all duration-30000 font-mono"
+		class="dark:fill-dark-ow dark:text-dark-ow text-light-black transition-all duration-30000 font-mono overflow-x-hidden"
 	>
 		<Navbar />
 		<slot />
